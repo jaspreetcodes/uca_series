7369fbccd05c9801fb2486a1316af85e8cf526d3
vagrant <vagrant@myvm19>
2021-08-07 16:24:56 +0000

    Assignment 5 Commit

--- Assignment_5-Recursion/Question_2-GCD.c
+++ Assignment_5-Recursion/Question_2-GCD.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-int hcf(int n1, int n2);
-int main() {
-    int n1, n2;
-    printf("Enter two positive integers: ");
-    scanf("%d %d", &n1, &n2);
-    printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1, n2));
-    return 0;
-}
-
+//returns hcf
 int hcf(int n1, int n2) {
     if (n2 != 0)
         return hcf(n2, n1 % n2);
     else
         return n1;
+}
+
+//main
+int main() {
+    int n1, n2;
+    printf("Enter two nos: ");
+    scanf("%d %d", &n1, &n2);
+    printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1, n2));
+    return 0;
 }
